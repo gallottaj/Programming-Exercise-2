1d76b0b70ca1c285a81a54f7de10fc478642b102
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 15:48:03 -0800

    initial commit, test user input message

---
+++ Exercise2/src/CalculateSum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CalculateSum {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.print("enter a 5-digit positive integer");
+	}
+
+}

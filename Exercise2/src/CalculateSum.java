c7d37c41c22ac7ff0c26bd8f63dc7625da944911
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 15:55:51 -0800

    test user input with simple sum

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+
 public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);
-System.out.print("enter a 5-digit positive integer");
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter a number:");
+		double number = input.nextDouble();
+		double sum = number + number;
+		System.out.println("The sum is" + " " + sum);
 	}
-
 }

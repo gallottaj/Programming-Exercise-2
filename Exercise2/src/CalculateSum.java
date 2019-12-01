c49b8c10d9a41384dab6f93323d93ad1ad796991
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 17:53:39 -0800

    reformat

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -3,21 +3,18 @@ import java.util.Scanner;
 public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Scanner input = new Scanner(System.in);
-//		int number = input.nextInt();
-		int number = 12345;
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
 		
-        System.out.print("Sum of the digits of " + number + " is: " + getSum(number));
+		System.out.print("enter a number");
 	}
-		  public static int getSum(int number) {
-		        int sum = 0;
-		        while (number != 0) {
-		            sum = sum + number % 10;
-		            number = number/10;
+	
+	public static int getSum(int number) {
+		int sum = 0;
+		while (number != 0) {
+			sum = sum + number % 10;
+			number = number / 10;
+		}
+		return sum;
+	}
 }
-		  return sum; 
-		  }
-	}
-	
-	
-

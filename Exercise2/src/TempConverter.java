ec5704f005e0113c7fe5d189fdcf3a4f1d51a2c0
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 18:56:44 -0800

    final converter

--- Exercise2/src/TempConverter.java
+++ Exercise2/src/TempConverter.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class TempConverter {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner input_f = new Scanner(System.in);
 		System.out.print("Enter a Fahrenheit temperature: ");
-		int fahrenheit = input.nextInt();
+		int fahrenheit = input_f.nextInt();
 		int convert_to_celcius = ((fahrenheit - 32) * 5 / 9);
-		System.out.print(fahrenheit + "F" + " " + "is equivilent to" + " " + convert_to_celcius + "C");
+		System.out.println(fahrenheit + "F" + " " + "is equivilent to" + " " + convert_to_celcius + "C");
+
+		Scanner input_c = new Scanner(System.in);
+		System.out.print("Enter a Celcius temperature: ");
+		int celcius = input_c.nextInt();
+		int convert_to_fahrenheit = ((celcius * 9 / 5) + 32);
+		System.out.print(celcius + "C" + " " + "is equivilent to" + " " + convert_to_fahrenheit + "F");
 	}
 }

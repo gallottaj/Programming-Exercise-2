2595d9f2ec7e1722285fe2a5cc0c924903e8a3be
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 17:50:25 -0800

    hard code to return sum of number

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter a 5-digit positive integer:");
-		int number = input.nextInt();
-		if (number % 2 == 0)
-			System.out.println("I am even");
-		else
-			System.out.println("I am NOT even");
+//		Scanner input = new Scanner(System.in);
+//		int number = input.nextInt();
+		int number = 12345;
+		
+        System.out.print("Sum of the digits of " + number + " is: " + getSum(number));
 	}
+		  public static int getSum(int number) {
+		        int sum = 0;
+		        while (number != 0) {
+		            sum = sum + number % 10;
+		            number = number/10;
 }
+		  return sum; 
+		  }
+	}
+	
+	
+

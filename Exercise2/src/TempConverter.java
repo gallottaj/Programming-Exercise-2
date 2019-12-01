e062fa1a54f75b76c35f2cc7f1cdeacc1d9e47f5
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 18:35:51 -0800

    enter a fahrenheit temperature

--- Exercise2/src/TempConverter.java
+++ Exercise2/src/TempConverter.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class TempConverter {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a Fahrenheit temperature: ");
+		int fahrenheit = input.nextInt();
 	}
 }

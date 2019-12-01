62b54f13b5c3cb8cd0ecd11bb30ccfc70b803048
Joe Gallotta <joeygallotta@gmail.com>
2019-11-30 16:06:58 -0800

    test if number is even or odd

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -4,9 +4,11 @@ public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.print("enter a number:");
-		double number = input.nextDouble();
-		double sum = number + number;
-		System.out.println("The sum is" + " " + sum);
+		System.out.print("Enter a 5-digit positive integer:");
+		int number = input.nextInt();
+		if (number % 2 == 0)
+			System.out.println("I am even");
+		else
+			System.out.println("I am NOT even");
 	}
 }

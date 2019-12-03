f309fc91631c27779e619ec7cb953844b7b1c1df
Joe Gallotta <joeygallotta@gmail.com>
2019-12-03 15:20:30 -0800

    format

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -6,12 +6,13 @@ public class CalculateSum {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter a 5-digit positive integer:");
 		int input = in.nextInt();
-        int n2 = input / 10000 % 10;
-        int n3 = input / 1000 % 10;
-        int n4 = input / 100 % 10;
-        int n5 = input / 10 % 10;
-        int n6 = input % 10;
-      System.out.println("The sum of the digits is" + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6 + " " + "=" + " " + getSum(input));
+		int n2 = input / 10000 % 10;
+		int n3 = input / 1000 % 10;
+		int n4 = input / 100 % 10;
+		int n5 = input / 10 % 10;
+		int n6 = input % 10;
+		System.out.println("The sum of the digits is" + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6 + " " + "="
+				+ " " + getSum(input));
 
 	}
 

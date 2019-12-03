b231f36f73621de600af642bd942337d8ce9124d
Joe Gallotta <joeygallotta@gmail.com>
2019-12-02 21:19:43 -0800

    print out each digit plus total sum

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter a number:");
-		int number = input.nextInt();
-		System.out.print(getSum(number));
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a 5-digit positive integer:");
+		int input = in.nextInt();
+        int n2 = input / 10000 % 10;
+        int n3 = input / 1000 % 10;
+        int n4 = input / 100 % 10;
+        int n5 = input / 10 % 10;
+        int n6 = input % 10;
+      System.out.println("The sum of the digits is" + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6 + " " + "=" + " " + getSum(input));
 
 	}
 
-	private static int getSum(int number) {
+	private static int getSum(int in) {
 		int sum = 0;
-		while (number != 0) {
-			sum = sum + number % 10;
-			number = number / 10;
+		while (in != 0) {
+			sum = sum + in % 10;
+			in = in / 10;
 		}
 		return sum;
 	}

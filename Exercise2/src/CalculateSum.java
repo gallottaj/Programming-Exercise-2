5808aa3dfbf72e12603ce480902b54a30c30e57e
Joe Gallotta <joeygallotta@gmail.com>
2019-12-03 15:43:58 -0800

    add conditional with error message

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -6,13 +6,19 @@ public class CalculateSum {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter a 5-digit positive integer:");
 		int input = in.nextInt();
-		int n2 = input / 10000 % 10;
-		int n3 = input / 1000 % 10;
-		int n4 = input / 100 % 10;
-		int n5 = input / 10 % 10;
-		int n6 = input % 10;
-		System.out.println("The sum of the digits is" + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6 + " " + "="
-				+ " " + getSum(input));
+		if ((input < 10000) || (input > 99999 || input < 0)) {
+			// error message
+			System.out.println("Invalid format");
+		} else {
+			int num_1 = input / 10000 % 10;
+			int num_2 = input / 1000 % 10;
+			int num_3 = input / 100 % 10;
+			int num_4 = input / 10 % 10;
+			int num_5 = input % 10;
+
+			System.out.println("The sum of the digits is" + " " + num_1 + " " + "+" + " " + num_2 + " " + "+" + " "
+					+ num_3 + " " + "+" + " " + num_4 + " " + "+" + " " + num_5 + " " + "=" + " " + getSum(input));
+		}
 
 	}
 

3f5c96197111f8dd93cf8859326a3500ab574901
Joe Gallotta <joeygallotta@gmail.com>
2019-12-02 20:59:02 -0800

    return sum is a success

--- Exercise2/src/CalculateSum.java
+++ Exercise2/src/CalculateSum.java
@@ -4,10 +4,13 @@ public class CalculateSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number:");
 		int number = input.nextInt();
-		System.out.print("Enter a number:");}	
-	
-	public static int getSum(int number) {
+		System.out.print(getSum(number));
+
+	}
+
+	private static int getSum(int number) {
 		int sum = 0;
 		while (number != 0) {
 			sum = sum + number % 10;
